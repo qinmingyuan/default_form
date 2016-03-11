39651282a3e6aaea0589b1d26fe7771a153a00b0
qinmingyuan <mingyuan0715@foxmail.com>
2016-03-11 16:51:35 +0800

    better name

--- lib/default_form.rb
+++ lib/default_form.rb
@@ -5,9 +5,11 @@ require 'default_form/config/search'
 
 class ActiveRecord::Base
 
-  def self.enum_trans(attribute)
-    h = I18n.t "activerecord.attributes.#{self.model_name.i18n_key}/#{attribute}"
+  def self.options_i18n(attribute)
+    h = I18n.t "#{self.i18n_scope}.attributes.#{self.model_name.i18n_key}/#{attribute}"
     h.invert
   end
+
+
 
 end
