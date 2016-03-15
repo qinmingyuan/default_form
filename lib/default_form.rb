f8b1fa5b43989dfb442f61c97ce5e905ce3952cf
qinmingyuan <mingyuan0715@foxmail.com>
2016-03-15 13:27:26 +0800

    hash with symble

--- lib/default_form.rb
+++ lib/default_form.rb
@@ -19,7 +19,7 @@ class ActiveRecord::Base
     h = I18n.t "#{self.i18n_scope}.attributes.#{self.model_name.i18n_key}/#{attribute}"
 
     if h.is_a? Hash
-      h[value]
+      h[value.to_sym]
     else
       human_attribute_name(value)
     end
