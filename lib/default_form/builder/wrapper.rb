a69e874abdafe5bf996a8317640ea0e41dcbe0bb
qinmingyuan <mingyuan0715@foxmail.com>
2016-09-27 17:07:08 +0800

    config support hash

--- lib/default_form/builder/wrapper.rb
+++ lib/default_form/builder/wrapper.rb
@@ -2,37 +2,37 @@ module DefaultForm::Builder
   module Wrapper
 
     def label(method, text = nil, options = {}, &block)
-      options[:class] ||= css.label
+      options[:class] ||= css[:label]
       super
     end
 
     def wrapper_all(inner)
-      if on.wrapper_all
-        content_tag(:div, inner, class: css.wrapper_all)
+      if on[:wrapper_all]
+        content_tag(:div, inner, class: css[:wrapper_all])
       else
         inner
       end
     end
 
     def wrapper_input(inner)
-      if on.wrapper_input
-        content_tag(:div, inner, class: css.wrapper_input)
+      if on[:wrapper_input]
+        content_tag(:div, inner, class: css[:wrapper_input])
       else
         inner
       end
     end
 
     def wrapper_submit(inner)
-      if on.wrapper_submit
-        content_tag(:div, inner, class: css.wrapper_submit)
+      if on[:wrapper_submit]
+        content_tag(:div, inner, class: css[:wrapper_submit])
       else
         inner
       end
     end
 
     def offset(text = '')
-      if on.offset
-        content_tag(:label, text, class: css.offset)
+      if on[:offset]
+        content_tag(:label, text, class: css[:offset])
       else
         ''
       end
