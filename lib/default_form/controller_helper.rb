9064795430c53fdbaccaa6dc15defb689a444ea6
qinmingyuan <mingyuan0715@foxmail.com>
2017-03-06 15:24:05 +0800

    remove a unused code

--- lib/default_form/controller_helper.rb
+++ lib/default_form/controller_helper.rb
@@ -4,7 +4,6 @@ module DefaultForm::ControllerHelper
     def model.default_form_builder(builder, &block)
       if builder.is_a? String
         Object.const_set builder, Class.new(DefaultForm::DefaultBuilder)
-        builder.constantize.include ActiveSupport::Configurable
         builder.constantize.config.on = ActiveSupport::OrderedOptions.new
         builder.constantize.config.css = ActiveSupport::OrderedOptions.new
 
