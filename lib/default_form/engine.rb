53c953ac5a123b2c28e3a18c8a726d322580dd0f
qinmingyuan <mingyuan0715@foxmail.com>
2017-04-25 10:55:35 +0800

    remove repeat codes

--- lib/default_form/engine.rb
+++ lib/default_form/engine.rb
@@ -1,9 +1,5 @@
 module DefaultForm
   class Engine < ::Rails::Engine
 
-    initializer 'default_form.add_generator_templates' do |app|
-      app.config.assets.paths.push File.expand_path('../../assets/javascripts', __FILE__)
-    end
-
   end
 end
