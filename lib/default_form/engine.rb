d5a8ab54c784296e3b622841afeec4c9818148d0
qinmingyuan <mingyuan0715@foxmail.com>
2017-07-05 14:25:52 +0800

    buidle gem v1.2

--- lib/default_form/engine.rb
+++ lib/default_form/engine.rb
@@ -2,4 +2,8 @@ module DefaultForm
   class Engine < ::Rails::Engine
 
   end
+
+  module Builder
+
+  end
 end
--- lib/default_form/version.rb
+++ lib/default_form/version.rb
@@ -1,9 +1,5 @@
 module DefaultForm
-  VERSION = '1.1.0'
+  VERSION = '1.2.0'
 end
 
-module DefaultForm
-  module Builder
 
-  end
-end
