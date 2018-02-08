0c7cf6f1cc87809ac99b4740cda1a7c7bdfa156f
qinmingyuan <mingyuan0715@foxmail.com>
2018-02-08 15:07:34 +0800

    object name

--- lib/default_form/view_helper.rb
+++ lib/default_form/view_helper.rb
@@ -8,14 +8,19 @@ module DefaultForm::ViewHelper
     form_for(record, options, &block)
   end
 
-  def search_form_for(record = :q, options = {}, &block)
-    options[:builder] = DefaultForm::SearchBuilder
-    form_for(record, options, &block)
-  end
-
   def default_form_with(**options, &block)
     options[:builder] = DefaultForm::DefaultBuilder
     form_with(options, &block)
+  end
+
+  def default_form_object(record, options = {})
+    object_name = options[:as] || record.class.base_class.model_name.param_key
+    DefaultForm::DefaultBuilder.new(object_name, record, self, options)
+  end
+
+  def search_form_for(record = :q, options = {}, &block)
+    options[:builder] = DefaultForm::SearchBuilder
+    form_for(record, options, &block)
   end
 
   def search_form_with(**options, &block)
