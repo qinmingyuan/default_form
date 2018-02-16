7d1a44f0a01b01c6384bd010a32301d628d7faf2
qinmingyuan <mingyuan0715@foxmail.com>
2018-02-16 23:13:36 +0800

    upgrade examples with bootstrap;

--- examples/search_bootstrap.rb
+++ examples/search_bootstrap.rb
@@ -1,12 +1,15 @@
 SearchForm.configure do |config|
+  config.css.form = 'form-inline'
+
   config.on.wrapper_all = false
-  config.on.wrapper_input = true
-  config.on.wrapper_submit = false
+  config.css.wrapper_all = 'inline field'
+
   config.on.offset = false
 
-  config.css.form = 'ui form'
-  config.css.wrapper_all = 'inline field'
-  config.css.wrapper_input = 'field'
+  config.on.wrapper_input = false
+  config.css.wrapper_input = 'col-sm-10'
+
+  config.on.wrapper_submit = false
   config.css.wrapper_submit = ''
   config.css.offset = ''
   config.css.label = ''
@@ -14,3 +17,5 @@ SearchForm.configure do |config|
   config.css.select = 'ui dropdown'
   config.css.submit = 'ui button'
 end
+
+
