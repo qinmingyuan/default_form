490506ba592e8505fc9d11df0e51b600fcc54cb2
qinmingyuan <mingyuan0715@foxmail.com>
2018-02-17 09:12:02 +0800

    unify search and default form config

--- lib/default_form/config/search.rb
+++ lib/default_form/config/search.rb
@@ -13,21 +13,26 @@ module SearchForm
     config.css.wrapper_all = nil
     config.css.wrapper_all_error = nil
 
-    config.on.wrapper_input = true
-    config.css.wrapper_input = 'field'
-    config.css.input = nil
-    config.css.select = nil
-    config.css.muilti_select = nil
-    config.css.checkbox = nil
-
-    config.on.wrapper_submit = false
-    config.css.wrapper_submit = nil
-    config.css.submit = 'ui button'
-
     config.on.offset = false
     config.css.offset = nil
     config.css.label = nil
 
+    config.on.wrapper_input = true
+    config.css.wrapper_input = 'field'
+    config.css.input = nil
+    config.css.select = nil
+    config.css.multi_select = nil
+
+    config.on.wrapper_checkbox = true
+    config.css.checkbox = nil
+    config.css.wrapper_checkbox = 'ui toggle checkbox'
+    config.css.wrapper_checkbox_checked = 'ui teal label checkbox'
+    config.css.wrapper_inline_checkbox = 'ui label checkbox'
+
+    config.on.wrapper_submit = false
+    config.css.wrapper_submit = nil
+    config.css.submit = 'ui button'
+    
     config.on.autocomplete = true
     config.on.skip_default_ids = false
 
