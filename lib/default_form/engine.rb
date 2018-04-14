dfc39769c839cbdf12ceecefd44781e7eca4c27c
qinmingyuan <mingyuan0715@foxmail.com>
2018-04-14 10:53:30 +0800

    avoid field error proc

--- lib/default_form/engine.rb
+++ lib/default_form/engine.rb
@@ -1,6 +1,10 @@
 module DefaultForm
   class Engine < ::Rails::Engine
 
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      html_tag
+    end
+    
   end
 
   module Builder
