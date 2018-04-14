6a2b8390099eda705201fab94f93906e3ca69ebc
qinmingyuan <mingyuan0715@foxmail.com>
2018-04-14 16:48:29 +0800

    upgrade weui

--- examples/valid_weui.js
+++ examples/valid_weui.js
@@ -1,11 +1,6 @@
-function validRequired(self_dom) {
+function validForm(self_dom) {
   $(self_dom).parent().parent().addClass('weui-cell_warn');
   $(self_dom).parent().parent().append('<i class="weui-icon-warn"></i>');
-}
-
-function validPattern(self_dom) {
-  $(self_dom).parent().parent().removeClass('weui-cell_warn');
-  $(self_dom).parent().next('.weui-icon-warn').remove();
 }
 
 function clearValid(self_dom) {
