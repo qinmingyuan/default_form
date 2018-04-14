d4c39a73edcd7556f2dfcc6094a101d1c4d53973
qinmingyuan <mingyuan0715@foxmail.com>
2018-04-14 10:54:24 +0800

    defautl html tag

--- lib/default_form/engine.rb
+++ lib/default_form/engine.rb
@@ -1,10 +1,8 @@
 module DefaultForm
   class Engine < ::Rails::Engine
 
-    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
-      html_tag
-    end
-    
+    config.action_view.field_error_proc = ->(html_tag, instance){ html_tag }
+
   end
 
   module Builder
