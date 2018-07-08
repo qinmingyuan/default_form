5ff91f2e8b7196e0487e239c6ef7a201e3c4a3f6
qinmingyuan <mingyuan0715@foxmail.com>
2018-07-08 16:35:19 +0800

    should sep valid js

--- examples/valid_weui.js
+++ examples/valid_weui.js
@@ -1,9 +1,11 @@
-function validForm(self_dom) {
-  $(self_dom).parent().parent().addClass('weui-cell_warn');
-  $(self_dom).parent().parent().append('<i class="weui-icon-warn"></i>');
+HTMLInputElement.prototype.validForm = function() {
+  var self = $(this)
+  self.parent().parent().addClass('weui-cell_warn');
+  self.parent().parent().append('<i class="weui-icon-warn"></i>');
 }
 
-function clearValid(self_dom) {
-  $(self_dom).parent().parent().removeClass('weui-cell_warn');
-  $(self_dom).parent().next('.weui-icon-warn').remove();
+HTMLInputElement.prototype.clearValid = function() {
+  var self = $(this)
+  self.parent().parent().removeClass('weui-cell_warn');
+  self.parent().next('.weui-icon-warn').remove();
 }
