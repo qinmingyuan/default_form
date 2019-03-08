ca9a67be1ce796ab68ebd4ceb592310b2c49ced7
qinmingyuan <mingyuan0715@foxmail.com>
2019-03-08 11:48:09 +0800

    add method define formb uilder

--- lib/default_form/controller_helper.rb
+++ lib/default_form/controller_helper.rb
@@ -1,18 +1,27 @@
 module DefaultForm::ControllerHelper
 
   def self.prepended(model)
+
     def model.default_form_builder(builder, &block)
+      builder = define_form_builder(builder, &block)
+      super builder
+    end
+
+    def model.define_form_builder(builder, &block)
       if builder.is_a? String
         Object.const_set builder, Class.new(DefaultForm::DefaultBuilder)
-        builder.constantize.config.on = ActiveSupport::OrderedOptions.new
-        builder.constantize.config.css = ActiveSupport::OrderedOptions.new
+        builder_class = builder.constantize
+        builder_class.config.on = ActiveSupport::OrderedOptions.new
+        builder_class.config.css = ActiveSupport::OrderedOptions.new
 
         if block_given?
-          yield builder.constantize.config
+          yield builder_class.config
         end
+
+        builder_class
       end
-      super
     end
+
   end
 
 end
