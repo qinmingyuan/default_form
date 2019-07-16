5ccfd19c7c3c063f562a7f4e4b160596010c658d
qinmingyuan <mingyuan0715@foxmail.com>
2019-07-16 21:07:11 +0800

    simple regexp

--- lib/default_form/builder/wrapper.rb
+++ lib/default_form/builder/wrapper.rb
@@ -144,8 +144,8 @@ module DefaultForm::Builder::Wrapper
   end
 
   def wrapper_id(method)
-    on = @object_name.gsub(/\]\[|[^a-zA-Z0-9]/, '_').sub(/_$/, '')
-    m = method.to_s.gsub(/[^a-zA-Z0-9]/, '_').sub(/[_?]$/, '')
+    on = @object_name.gsub(/\]\[|\W/, '_').sub(/_$/, '')
+    m = method.to_s.gsub(/\W/, '_').sub(/[_?]$/, '')
     [on, m].join('_')
   end
 
