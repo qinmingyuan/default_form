8a65e56ebda08c4a6c9eff93d5387e3b621a0586
qinmingyuan <mingyuan0715@foxmail.com>
2019-07-17 14:25:51 +0800

    simple form object method

--- lib/default_form/view_helper.rb
+++ lib/default_form/view_helper.rb
@@ -5,19 +5,22 @@ require 'default_form/search_builder'
 
 module DefaultForm::ViewHelper
 
-  def form_object(record, options ={})
-    object_name = options[:as] || record.class.base_class.model_name.param_key
-    ActionView::Helpers::FormBuilder.new(object_name, record, self, options)
+  def form_object(record = nil, builder: DefaultForm::DefaultBuilder, **options)
+    object_name = options[:as]
+    unless object_name
+      if record.is_a?(ActiveRecord::Base)
+        object_name = record.class.base_class.model_name.param_key
+      else
+        object_name = ''
+      end
+    end
+    builder.new(object_name, record, self, options)
   end
+  alias_method :default_form_object, :form_object
 
   def default_form_with(**options, &block)
     options[:builder] ||= DefaultForm::DefaultBuilder
     form_with(options, &block)
-  end
-
-  def default_form_object(record = nil, **options)
-    object_name = options[:as] || record.class.base_class.model_name.param_key
-    DefaultForm::DefaultBuilder.new(object_name, record, self, options)
   end
 
   def search_form_with(**options, &block)
@@ -25,11 +28,6 @@ module DefaultForm::ViewHelper
     options[:scope] = '' unless options.key?(:scope)
     options[:url] ||= url_for
     form_with(options, &block)
-  end
-
-  def search_form_object(record = nil, **options)
-    object_name = options[:as] || ''
-    DefaultForm::SearchBuilder.new(object_name, record, self, options)
   end
 
 end
