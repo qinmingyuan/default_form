fca6c8c1ddba9002b622e1c0b4c85b09187e45fb
qinmingyuan <mingyuan0715@foxmail.com>
2019-11-21 14:41:55 +0800

    fix load err

--- lib/default_form/search_builder.rb
+++ lib/default_form/search_builder.rb
@@ -21,10 +21,10 @@ class DefaultForm::SearchBuilder < ActionView::Helpers::FormBuilder
       object.assign_attributes _values.slice(*object.attribute_names)
     end
 
-    options[:skip_default_ids] = origin_on.skip_default_ids
+    options[:skip_default_ids] = origin_on[:skip_default_ids]
     options[:local] ||= true unless options[:remote]
     options[:method] ||= 'get'
-    options[:class] = origin_css.form unless options.key?(:class)
+    options[:class] = origin_css[:form] unless options.key?(:class)
 
     super
   end
