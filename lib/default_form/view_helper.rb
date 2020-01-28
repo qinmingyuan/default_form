80cf6bfdc6382aa5ee0daa4ac0cca80a40fadd1d
qinmingyuan <mingyuan0715@foxmail.com>
2020-01-28 19:19:19 +0800

    suport search

--- lib/default_form/view_helper.rb
+++ lib/default_form/view_helper.rb
@@ -5,20 +5,20 @@ module DefaultForm::ViewHelper
 
   def form_object(record = nil, builder: DefaultForm::FormBuilder, **options)
     object_name = options[:as].to_s
-    
+
     if object_name.blank? && record.is_a?(ActiveRecord::Base)
       object_name = record.class.base_class.model_name.param_key
     end
-    
+
     builder.new(object_name, record, self, options)
   end
 
   # theme: :default
   def form_with(**options, &block)
     unless options.key?(:scope)
-      options[:scope] = '' if options[:theme] == 'search'
+      options[:scope] = '' if options[:theme].end_with? 'search'
     end
-    if options[:theme] == 'search'
+    if options[:theme].end_with? 'search'
       options[:url] = url_for unless options.key?(:url)
     end
     options[:theme] = 'default' unless options.key?(:theme)
@@ -28,7 +28,7 @@ module DefaultForm::ViewHelper
 
   def form_for(record, options = {}, &block)
     options[:theme] = 'default' unless options.key?(:theme)
-    
+
     super
   end
 
