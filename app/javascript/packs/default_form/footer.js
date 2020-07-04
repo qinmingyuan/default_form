a9cf6256492aa6d897a3e2b98271c302690941a1
qinmingyuan <mingyuan0715@foxmail.com>
2020-07-04 20:21:23 +0800

    default form

--- app/javascript/packs/default_form/footer.js
+++ app/javascript/packs/default_form/footer.js
@@ -1,4 +1,4 @@
-export function supportFormValid() {
+export function prepareFormValid() {
   document.querySelectorAll('textarea[data-valid], input[data-valid]').forEach(el => {
     el.addEventListener('blur', () => { this.checkValidity() })
     el.addEventListener('input', () => { this.defaultFormClear() })
