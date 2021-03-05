75de8d5d009a86c942e7186e0e76b4fa874b408a
qinmingyuan <mingyuan0715@foxmail.com>
2021-03-05 18:42:00 +0800

    set cloned value null

--- app/packs/entrypoints/default_form/field.js
+++ app/packs/entrypoints/default_form/field.js
@@ -23,6 +23,7 @@ class FieldController extends Controller {
     el.querySelectorAll('input, select').forEach(input => {
       input.name = input.name.replace(`[${this.indexValue}]`, `[${nextIndex}]`)
       input.id = input.id.replace(`${this.indexValue}`, `${nextIndex}`)
+      input.value = null
     })
 
     if (this.element.parentNode) {
